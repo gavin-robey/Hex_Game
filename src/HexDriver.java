40df7cbb9cb3ada0263b73d470550afa29ae7dc3
Gavin Robey <gavin.robey15@gmail.com>
2023-03-07 19:40:33 -0700

    created gradle.build file

--- src/HexDriver.java
+++ src/HexDriver.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class Driver {
+public class HexDriver{
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";    
